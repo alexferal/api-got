d9dbd8e631185a2566a863d9342dfad26bc48c41
Alex Fernandes <almeidafer92@gmail.com>
2020-03-11 14:31:40 -0300

    first commit

---
+++ src/main/java/com/alex/apigot/ApigotApplication.java
@@ -0,0 +1,13 @@
+package com.alex.apigot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/apigot/ApigotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.apigot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
